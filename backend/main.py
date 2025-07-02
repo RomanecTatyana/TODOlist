b1f97791fe165a3c944cee4a06bc94d603169b86
Romanec Tatyana <romanectatyana@gmail.com>
2025-07-02 22:59:12 +0300

    added endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,40 @@
 from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
 
 class Task(BaseModel):
     id: int
     title: str
-    status: bool
-    
+    complited: bool
+
+#create a list for saving task items   
 tasks = []
 
+@app.get("/tasks", summary="Отримати всі задачі")
+def read_tasks():
+    return tasks
 
+
+@app.post("/tasks", summary="Створити нову задачу")
+def create_task(task: Task):
+    tasks.append(task)
+    return {"message": "Task added succesfully"}
+
+
+@app.put("/tasks/{id}", summary="Оновити задачу")
+def update_task(id: int, updated_task: Task):
+    for index, task in enumerate(tasks):
+        if task.id == id:
+            tasks[index] = updated_task
+            return {"message": "Task updated successfully"}
+    return {"error": "Task not found"}
+
+
+@app.delete("/tasks/{id}", summary="Видалити задачу")
+def delete_task(id: int):
+    for index, task in enumerate(tasks):
+        if task.id == id:
+            tasks.pop(index)
+            return {"message": "Task deleted successfully"}
+    return {"error": "Task not found"}
